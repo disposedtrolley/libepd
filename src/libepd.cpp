c177f64ee07d474913ad1b414cd0cb78a46e433b
James Liu <james_liu@fastmail.com>
2020-09-05 15:56:17 +1000

    feat: add SPI, GPIO, and EPaper classes

--- src/libepd.cpp
+++ src/libepd.cpp
@@ -1,10 +1,18 @@
-#include <iostream>
 #include "libepd/libepd.hpp"
 
-using namespace std;
+namespace libepd {
+    EPaper::EPaper(GPIOManager *busy_pin, GPIOManager *reset_pin, SPIManager *spi, void (*delay)(int milliseconds)) {
+        this->busy_pin = busy_pin;
+        this->reset_pin = reset_pin;
+        this->spi = spi;
+        this->delay = delay;
+    }
 
-int foo() {
-    cout << "Hello, World" << endl;
-
-    return 0;
+    void EPaper::reset() {
+        this->reset_pin->set(1);
+        this->delay(200);
+        this->reset_pin->set(0);
+        this->delay(200);
+        this->reset_pin->set(1);
+    }
 }
