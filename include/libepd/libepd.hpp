c6410da8b838b396a4e07c62b47e05ff6b1e2e4c
James Liu <james_liu@fastmail.com>
2020-09-05 15:56:17 +1000

    feat: add SPI, GPIO, and EPaper classes

--- include/libepd/libepd.hpp
+++ include/libepd/libepd.hpp
@@ -1,6 +1,32 @@
 #ifndef LIBEPD_LIBEPD_H
 #define LIBEPD_LIBEPD_H
 
-int foo();
+namespace libepd {
+    class SPIManager {
+    public:
+        virtual void send() = 0;
+    };
+
+    class GPIOManager {
+    public:
+        virtual void set(int logic_level) = 0;
+        virtual int get() = 0;
+    };
+
+    class EPaper {
+        GPIOManager *busy_pin;
+        GPIOManager *reset_pin;
+        SPIManager *spi;
+        void (*delay)(int milliseconds);
+
+    public:
+        EPaper(GPIOManager *busy_pin,
+               GPIOManager *reset_pin,
+               SPIManager *spi,
+               void (*delay)(int milliseconds));
+
+        void reset();
+    };
+}
 
 #endif //LIBEPD_LIBEPD_H
--- tests/test.cpp
+++ tests/test.cpp
@@ -2,6 +2,44 @@
 #include "catch2/catch.hpp"
 #include "libepd/libepd.hpp"
 
-TEST_CASE( "Foo" ) {
-    foo();
+using namespace std;
+using namespace libepd;
+
+class MockResetPinManager : public GPIOManager {
+    vector<int> level_history{};
+
+public:
+    MockResetPinManager(){};
+
+    void set(int logic_level) {
+        this->level_history.push_back(logic_level);
+    }
+
+    int get() {
+        return this->level_history[this->level_history.size() - 1];
+    }
+
+    vector<int> get_level_history() {
+        return this->level_history;
+    }
+};
+
+void mock_delay(int milliseconds) {};
+
+TEST_CASE("Test device reset") {
+    MockResetPinManager reset_pin_mgr = MockResetPinManager();
+
+    EPaper epd = EPaper(nullptr, &reset_pin_mgr, nullptr, mock_delay);
+
+    epd.reset();
+
+    // Reset pin should be pulled back high after resetting.
+    REQUIRE(reset_pin_mgr.get() == 1);
+
+    // Reset pin should've been pulled low once.
+    int count_low_states{0};
+    for (int level : reset_pin_mgr.get_level_history()) {
+        if (level == 0) count_low_states++;
+    }
+    REQUIRE(count_low_states == 1);
 }
